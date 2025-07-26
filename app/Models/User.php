b90cf8d1670b92837250d38790a23c048402c531
akihide-hatano <a.h.19841202@gmail.com>
2025-07-26 12:11:13 +0900

    Models/User.phpにてポケモンのお気に入り機能の追加、取得、削除機能を実装しました。

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\DB;
 
 class User extends Authenticatable
 {
@@ -44,5 +45,60 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    // ユーザーがお気に入りのポケモンIDを取得する
+    public function getFavoritePokemonIds(): array
+    {
+        return DB::table('favorite_pokemons')
+                ->where('user_id',$this->id)
+                ->pluck('pokemon_api_id')
+                ->toArray();
+    }
+
+    //指定されたポケモンをお気に入りに追加する
+    public function addFavoritePokemon( int $pokemonApiId):bool
+    {
+        if( ! DB::table('favorite_pokemons')
+                ->where('user_id',$this->id)
+                ->where('pokemon_api_id',$pokemonApiId)
+                ->exists()){
+            DB::table('favorite_pokemons')->insert([
+                'user_id'=>$this->id,
+                'pokemon_api_id'=> $pokemonApiId,
+                // 'created_at' => now(), // もしtimestamps()をマイグレーションで有効にしたら、これらも追加
+                // 'updated_at' => now(),
+            ]);
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 指定されたポケモンをお気に入りから削除する。
+     *
+     * @param int $pokemonApiId PokeAPIのポケモンID
+     * @return int 削除された行数
+     */
+    public function removeFavoritePokemon(int $pokemonApiId): int
+    {
+        return DB::table('favorite_pokemons')
+            ->where('user_id', $this->id)
+            ->where('pokemon_api_id', $pokemonApiId)
+            ->delete();
+    }
+
+    /**
+     * 指定されたポケモンがこのユーザーのお気に入りに追加されているか確認する。
+     *
+     * @param int $pokemonApiId PokeAPIのポケモンID
+     * @return bool
+     */
+    public function isFavoritePokemon(int $pokemonApiId): bool
+    {
+        return DB::table('favorite_pokemons')
+                  ->where('user_id', $this->id)
+                  ->where('pokemon_api_id', $pokemonApiId)
+                  ->exists();
     }
 }
